51fba045336ee3308a871e088da6f75559fcabe1
awarrier1221 <112609052+awarrier1221@users.noreply.github.com>
2022-09-01 14:54:39 +0530

    Add files via upload
    Adding mp code

---
+++ amo/amo.s
@@ -0,0 +1,44 @@
+    .text
+
+    .global _start
+_start:
+
+//setting up stack pointer
+la x13,stack_start
+csrr a0,mhartid
+li x14,1024
+addi x15,a0,1
+mul x16,x15,x14
+add x13,x13,x16
+add sp,x13,0
+
+li x11,1
+la x12,lock
+again:lw x9,0(x12)
+    bnez x9,again
+    amoswap.w.aq x9,x11,0(x12)
+    bnez x9,again
+
+//critical_section
+critical:la x9,main
+jalr x9
+
+release: 
+amoswap.w.rl x0,x0,0(x12)
+
+exit:
+    .data
+    .balign 0x10
+
+.balign 8
+lock:
+    .word 0
+
+
+.balign 8
+stack_start:
+    .space 4096         // Allocate 4k worth of stack
+stack_end:
+
+.end
+
---
+++ amo/build.sh
@@ -0,0 +1,13 @@
+IMG_NAME=ABC
+
+set -o xtrace
+set -e
+
+mkdir -p build
+
+riscv64-unknown-elf-g++ -x assembler-with-cpp -c -march=rv64imafdc -mcmodel=medany  -ggdb -o build/start.o amo.s
+riscv64-unknown-elf-g++ -c -march=rv64imafdc -mcmodel=medany -ggdb -o build/main.o uart.cpp
+riscv64-unknown-elf-g++ -o ${IMG_NAME}.elf -g -Xlinker -Map=output.map  -T linker.script -nostdlib build/start.o build/main.o #build/user.o
+riscv64-unknown-elf-objdump -xsD ${IMG_NAME}.elf > ${IMG_NAME}.dis
+
+set +o xtrace
